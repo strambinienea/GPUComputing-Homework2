97d8184bae36f5584df413a5c05fa533483f3778
Strambini <enea.strambini@\unitn.it>
2024-05-29 16:47:46 +0200

    feat: tile coalesced matrix transposition

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 # Compiler to use
 CC = nvcc
 # Compiler flags
-CFLAGS = -std=c++14 -g
 INCLUDE = -Iinclude
 
 BUILD_DIR := build
@@ -9,7 +8,7 @@ LIB_DIR := lib
 TARGET_DIR := bin
 
 # Name of your main cpp file
-MAIN = Main.cpp
+MAIN = Main.cu
 # Name of the executable
 EXECUTABLE = $(TARGET_DIR)/GPUComp_H2
 
--- src/Main.cu
+++ src/Main.cu
@@ -22,16 +22,62 @@ __global__ void naiveTranspose(const MATRIX_TYPE* matrix, MATRIX_TYPE* transpose
     int index = matrixSize * row + column;
     int transposedIndex = matrixSize * column + row;
 
-    if (index < matrixSize) {
+    if ( index < matrixSize * matrixSize && transposedIndex < matrixSize * matrixSize ) {
+
         transposedMatrix[transposedIndex] = matrix[index];
     }
+}
 
+__global__ void coalescedTiledTranspose(const MATRIX_TYPE* matrix, MATRIX_TYPE* transposedMatrix, int matrixSize, int tileSize) {
+	
+	extern __shared__ MATRIX_TYPE tile[];
+
+	int column = blockIdx.x * blockDim.x + threadIdx.x;
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+	int index = matrixSize * row + column;
+    	int transposedIndex = matrixSize * column + row;
+
+	if ( index < matrixSize * matrixSize ) {
+		
+		tile[column + row * tileSize] = matrix[index];	
+    	}
+	
+	__syncthreads();
+
+	if ( transposedIndex < matrixSize * matrixSize) {
+		transposedMatrix[transposedIndex] = tile[column + tileSize * row];
+	}
+}
+
+/**
+ * Initialize the matrix with random values
+ * @param matrix - The matrix to initialize
+ * @param matrixSize - The size of the matrix
+ */
+void initMatrix(MATRIX_TYPE* matrix, int matrixSize) {
+
+    for (int i = 0; i < matrixSize * matrixSize; i++) {
+        matrix[i] = (MATRIX_TYPE) rand() % MAX_RANDOM_VALUE;
+    }
+}
+
+void printMatrix(const MATRIX_TYPE* matrix, int matrixSize) {
+    
+	cout << "Matrix of " << matrixSize << " X " << matrixSize << " elements" << endl;
+	for (int i = 0; i < matrixSize * matrixSize; i++) {
+		
+		if (i % matrixSize == 0) { cout << endl;} 
+		cout << matrix[i] << " ";
+	}
+
+	cout << endl;
 }
 
 int main(int argc, char** argv) {
 
     // Check the size of the matrix is passed as an argument
-    if (argc < 4) {
+    if (argc < 3) {
         cout
         << "You must specify the size of the matrix and grid dimension as arguments"
         << endl;
@@ -51,10 +97,11 @@ int main(int argc, char** argv) {
     int GRID_SIZE = atoi(argv[2]);
 
     // Dimension of the tile matrix, each tile is a block
-    dim2 GRID_DIMENSION(GRID_SIZE, GRID_SIZE);
+    dim3 GRID_DIMENSION(GRID_SIZE, GRID_SIZE);
 
     // Dimension of a single tile or block
-    dim2 BLOCK_DIMENSION(MATRIX_SIZE / GRID_SIZE, MATRIX_SIZE / GRID_SIZE);
+    const int TILE_SIZE = MATRIX_SIZE / GRID_SIZE;
+    dim3 BLOCK_DIMENSION(TILE_SIZE, TILE_SIZE);
 
 
     MATRIX_TYPE *naiveA, *naiveB;
@@ -64,18 +111,19 @@ int main(int argc, char** argv) {
 
     initMatrix(naiveA, MATRIX_SIZE);
 
-    naiveTranspose<<GRID_DIMENSION, BLOCK_DIMENSION>>(naiveA, naiveB, MATRIX_SIZE);
+    if (debug) { printMatrix(naiveA, MATRIX_SIZE); }
+
+    cout << "Computing naive matrix transposition of a matrix of size "  
+	    << MATRIX_SIZE << " X " << MATRIX_SIZE << " and a grid of size " 
+	    << GRID_SIZE << " X " << GRID_SIZE << endl;
+    coalescedTiledTranspose<<<GRID_DIMENSION, BLOCK_DIMENSION, TILE_SIZE * TILE_SIZE * sizeof(MATRIX_TYPE)>>>(naiveA, naiveB, MATRIX_SIZE, TILE_SIZE);
+
+    cudaDeviceSynchronize();
+    if (debug) { printMatrix(naiveB, MATRIX_SIZE); }
+	
+    cudaFree(naiveA);
+    cudaFree(naiveB);
+
     return 0;
 }
 
-/**
- * Initialize the matrix with random values
- * @param matrix - The matrix to initialize
- * @param matrixSize - The size of the matrix
- */
-void initMatrix(*MATRIX_TYPE matrix, int matrixSize) {
-
-    for (int i = 0; i < matrixSize * matrixSize; i++) {
-        matrix[i] = (MATRIX_TYPE) rand() % MAX_RANDOM_VALUE;
-    }
-}
